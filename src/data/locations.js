8773e6edf19f924e002c8f1ea775e33dd2238b78
jsafe00 <josafebalili@gmail.com>
2022-12-20 14:21:24 +0800

    days

--- src/data/locations.js
+++ src/data/locations.js
@@ -1,6 +1,6 @@
 export const locations = [
   {
-    church: '2',
+    church: 'Day 1',
     placename: 'Church of Our Lady of Lourdes',
     info: 'https://drive.google.com/drive/folders/1lypAKy2j_V2YrZd7FNN85iQ1IEZb977G?usp=share_link',
     image: 'https://lh3.googleusercontent.com/8fgnSIS8KctcGa0o6AUmPLhA0kCTVqMwddwt54XFGZB9_n-04AgwK_l9X1qaqBUbxl4z7OKvzamdrc9mEbhrSLmZ7RkJKi-Gzh5RQaOYTNt6ekmBeId1xA8Twx_eRnJ3iwe02cdfns8=w2400',
@@ -13,7 +13,7 @@ export const locations = [
     ]
   },
   {
-    church: '3',
+    church: 'Day 2',
     placename: 'Our Lady of Perpetual Succour',
     info: 'https://drive.google.com/drive/folders/1mh6Gp0f6FxMDSAZk7dSrZWAVt5NhPGaE?usp=share_link',
     image: 'https://lh3.googleusercontent.com/t7jS9tbS1ifma3pTnSQLD8h-vSPHE70D7pUPguMDI-5B-9LJR36Xj-BWp9ZuJ3I6XEDIVoy6OvYH498U1PmzgdTvNzSu5D9QfYn2Zg-u5-MIMXCzth307OIecfLZ8GQ3sqiAbKgX2rU=w2400',
@@ -26,7 +26,7 @@ export const locations = [
     ]
   },
   {
-    church: '4',
+    church: 'Day 3',
     placename: 'Church of the Holy Spirit',
     info: 'https://drive.google.com/drive/folders/1pkZWTyv2aPLfpthWO58r23BSylAyu8iJ?usp=share_link',
     image: 'https://lh3.googleusercontent.com/YJ8ADTEnKJcOwFeSep2khTvOQV7XDoZ19NTcwSYdV-3i6Yuc081jIj5XhI03O3j20skV-ReeYotMd8vptV4Lr8IYSWDqWI5CGDOylBOSw_EIhsP2OVCcnwafby9IHd8ZPoi1kdodvAU=w2400',
@@ -39,7 +39,7 @@ export const locations = [
     ]
   },
   {
-    church: '5',
+    church: 'Day 4',
     placename: 'Church of the Risen Christ',
     info: 'https://drive.google.com/drive/folders/1vorHOnAfJ7IrEaCaLT6hiGuvYkq2WZe0?usp=share_link',
     image: 'https://lh3.googleusercontent.com/gFgnOiRb2o1KtERxwjvIbA-27nTCo-w8ZRJ1B9r5w0bkM2NO8IBIo5qPC5VEboVi2JrDlB6-ItauUkuDqxBZRISYO22DqcZQ4E-BjpD7vMQdDVEZFr1HH2mPq6ZGRduA3N0p8Ar4Oyc=w2400',
@@ -52,7 +52,7 @@ export const locations = [
     ]
   },
   {
-    church: '6',
+    church: 'Day 5',
     placename: 'Cathedral of the Good Shepherd',
     info: 'https://drive.google.com/drive/folders/1xbhXyT_pZ9OUimAZ6J1ZJNejHWm2IeeH?usp=share_link',
     image: 'https://lh3.googleusercontent.com/0Tft01NYAfmC0bTWqTK-b1WG-UU9lExWtOWTLPHCU4GSD_nvMO0a0eU-QkinucC2OQFvBIz33_TvpGO4a1snRPLz1q_kZI0mJEwJ-9AQVIifT0t27t76e-YUA5udETj1ADgcFUlC8W8=w2400',
